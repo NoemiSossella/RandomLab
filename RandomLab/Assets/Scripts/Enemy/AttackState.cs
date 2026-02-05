21d3c729596f255f9febd772ea7c001d9d502196
NoemiSossella <noemi.sossella@gmail.com>
2026-02-05 17:33:51 +0100

    AttackEnemy

---
+++ RandomLab/Assets/Scripts/Enemy/AttackState.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+public class AttackState : BaseSate
+{
+    private float moveTimer;
+    private float losePlayerTimer;
+
+    public override void Enter()
+    {
+        
+    }
+
+    public override void Perform()
+    {
+        if (enemy.CanSeePlayer())
+        {
+            losePlayerTimer = 0;
+            moveTimer += Time.deltaTime;
+            if (moveTimer > Random.Range(3, 7))
+            {
+                enemy.Agent.SetDestination(enemy.transform.position + (Random.insideUnitSphere * 5));
+                moveTimer = 0;
+            }
+        }
+        else
+        {
+            losePlayerTimer += Time.deltaTime;
+            if (losePlayerTimer > 8)
+            {
+                //cambia lo stato di ricerca
+            }
+        }
+    }
+
+    public override void Exit()
+    {
+        
+    }
+
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
